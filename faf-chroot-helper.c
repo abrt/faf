c660f98be7d57c081f2a1ebcd256aa9d7fa6701f
Karel Klic <kklic@redhat.com>
2011-11-23 11:21:35 +0100

    use AC_GNU_SOURCE

--- faf-chroot-helper.c
+++ faf-chroot-helper.c
@@ -18,13 +18,6 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
-#define _GNU_SOURCE
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>

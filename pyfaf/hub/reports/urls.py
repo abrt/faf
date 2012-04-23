b36e1de42793d976ab4e7dc66297269a32d2b240
Jakub Filak <jfilak@redhat.com>
2012-04-23 18:56:17 +0200

    reports list page initial commit
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- pyfaf/hub/reports/urls.py
+++ pyfaf/hub/reports/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('pyfaf.hub.reports.views',
     url(r'^$', 'index'),
     url(r'^list/$', 'list'),
-    url(r'^(?P<problem_id>\d+)/$', 'item'),
+    url(r'^(?P<report_id>\d+)/$', 'item'),
 )
--- pyfaf/hub/reports/views.py
+++ pyfaf/hub/reports/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from sqlalchemy import func
+import pyfaf
+from pyfaf.storage.problem import *
+from pyfaf.storage.report import *
 
 def index(request):
     return render_to_response('reports/index.html', {}, context_instance=RequestContext(request))
 
 def list(request):
-    return render_to_response('reports/list.html', {}, context_instance=RequestContext(request))
+    db = pyfaf.storage.getDatabase()
+    reports = db.session.query(Report.id, Report.first_occurence.label("created"), Report.last_occurence.label("last_change")).all();
+    return render_to_response('reports/list.html', {"reports":reports}, context_instance=RequestContext(request))
 
-def item(request):
-    return render_to_response('reports/item.html', {}, context_instance=RequestContext(request))
+def item(request, report_id):
+    db = pyfaf.storage.getDatabase()
+    report = db.session.query(Report).filter(Report.id==report_id)
+    return render_to_response('reports/item.html', {"report":report}, context_instance=RequestContext(request))

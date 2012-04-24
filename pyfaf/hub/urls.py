dbe14673415b41e8604e2f04fed656d5304905e0
Richard Marko <rmarko@redhat.com>
2012-04-24 11:04:56 +0200

    New design

--- pyfaf/hub/urls.py
+++ pyfaf/hub/urls.py
@@ -44,3 +44,6 @@ if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static('/js/flot', document_root='/usr/share/flot')
+    urlpatterns += static('/css/bootstrap', document_root='/usr/share/bootstrap/css')
+    urlpatterns += static('/img/bootstrap', document_root='/usr/share/bootstrap/img')
+    urlpatterns += static('/js/bootstrap', document_root='/usr/share/bootstrap/js')

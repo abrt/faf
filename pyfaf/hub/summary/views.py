5b83c892de520d9a8d0c92e8607593d2b38bd7a5
Richard Marko <rmarko@redhat.com>
2012-05-02 11:54:53 +0200

    dedup webui: summary views cleanup

--- pyfaf/hub/summary/views.py
+++ pyfaf/hub/summary/views.py
@@ -34,7 +34,8 @@ def index(request):
     duration_opt = form.get_duration_selection()
     component_ids = form.get_component_selection()
 
-    reports = ((name, release_incremental_history(db, ids, component_ids, duration_opt)) for ids, name in form.get_release_selection())
+    reports = ((name, release_incremental_history(db, ids, component_ids,
+        duration_opt)) for ids, name in form.get_release_selection())
 
     return render_to_response("summary/index.html",
                               { "reports": reports,
@@ -42,63 +43,63 @@ def index(request):
                                 "duration": duration_opt },
                               context_instance=RequestContext(request))
 
-def release_incremental_history(db, osreleases_id, component_ids, duration):
+def release_incremental_history(db, osrelease_ids, component_ids, duration):
     if duration == 'd':
         hist_table = ReportHistoryDaily
-        hist_column = ReportHistoryDaily.day
-        historyQuery = db.session.query(ReportHistoryDaily.day,
-            func.sum(ReportHistoryDaily.count)).\
-            filter(ReportHistoryDaily.day > datetime.date.today() - datetime.timedelta(days=15)).\
-            group_by(ReportHistoryDaily.day).\
-            order_by(ReportHistoryDaily.day)
+        history_query = (db.session.query(ReportHistoryDaily.day,
+            func.sum(ReportHistoryDaily.count)).
+            filter(ReportHistoryDaily.day > datetime.date.today() -
+                datetime.timedelta(days=15)).
+            group_by(ReportHistoryDaily.day).
+            order_by(ReportHistoryDaily.day))
     elif duration == 'w':
         hist_table = ReportHistoryWeekly
-        hist_column = ReportHistoryWeekly.week
-        historyQuery = db.session.query(ReportHistoryWeekly.week,
-            func.sum(ReportHistoryWeekly.count)).\
-            filter(ReportHistoryWeekly.week > datetime.date.today() - datetime.timedelta(weeks=9)).\
-            group_by(ReportHistoryWeekly.week).\
-            order_by(ReportHistoryWeekly.week)
+        history_query = (db.session.query(ReportHistoryWeekly.week,
+            func.sum(ReportHistoryWeekly.count)).
+            filter(ReportHistoryWeekly.week > datetime.date.today() -
+                datetime.timedelta(weeks=9)).
+            group_by(ReportHistoryWeekly.week).
+            order_by(ReportHistoryWeekly.week))
     else:
         hist_table = ReportHistoryMonthly
-        hist_column = ReportHistoryMonthly.month
         # duration == 'm'
-        historyQuery = db.session.query(ReportHistoryMonthly.month,
-            func.sum(ReportHistoryMonthly.count)).\
-            filter(ReportHistoryMonthly.month >= months_ago(12)).\
-            group_by(ReportHistoryMonthly.month).\
-            order_by(ReportHistoryMonthly.month)
+        history_query = (db.session.query(ReportHistoryMonthly.month,
+            func.sum(ReportHistoryMonthly.count)).
+            filter(ReportHistoryMonthly.month >= months_ago(12)).
+            group_by(ReportHistoryMonthly.month).
+            order_by(ReportHistoryMonthly.month))
 
-    if len(osreleases_id) != 0:
+    if osrelease_ids:
         #FIXME : correct selection of OS release !!
         #Missing table RepostOpSysReleaseHistory(Daily|Weekly|Monthly)
-        historyQuery = historyQuery.join(ReportOpSysRelease, ReportOpSysRelease.report_id==hist_table.report_id).\
-            filter(ReportOpSysRelease.opsysrelease_id.in_(osreleases_id))
+        history_query = (history_query.join(ReportOpSysRelease,
+                    ReportOpSysRelease.report_id==hist_table.report_id).
+            filter(ReportOpSysRelease.opsysrelease_id.in_(osrelease_ids)))
 
-    if len(component_ids) > 0:
+    if component_ids:
         # Selected Component
-        historyQuery = historyQuery.join(Report, OpSysComponent).\
-            filter(OpSysComponent.id.in_(component_ids))
+        history_query = (history_query.join(Report, OpSysComponent).
+            filter(OpSysComponent.id.in_(component_ids)))
 
-    historyDict = dict(historyQuery.all())
+    history_dict = dict(history_query.all())
 
     if duration == 'd':
         for i in range(0, 14):
             day = datetime.date.today() - datetime.timedelta(days=i)
-            if day not in historyDict:
-                historyDict[day] = 0
+            if day not in history_dict:
+                history_dict[day] = 0
     elif duration == 'w':
         for i in range(0, 8):
             day = datetime.date.today()
-            day -= datetime.timedelta(days=day.weekday()) + datetime.timedelta(weeks=i)
-            if day not in historyDict:
-                historyDict[day] = 0
+            day -= (datetime.timedelta(days=day.weekday()) +
+                datetime.timedelta(weeks=i))
+            if day not in history_dict:
+                history_dict[day] = 0
     else:
         # duration == 'm'
         for i in range(0, 12):
             day = months_ago(i)
-            if day not in historyDict:
-                historyDict[day] = 0
+            if day not in history_dict:
+                history_dict[day] = 0
 
-    return sorted(historyDict.items(), key=lambda x: x[0])
-
+    return sorted(history_dict.items(), key=lambda x: x[0])

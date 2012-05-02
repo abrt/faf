7cdd9a5f60ce2776af5e61911677e518de3093e4
Richard Marko <rmarko@redhat.com>
2012-05-02 12:36:12 +0200

    dedup webui: reports forms cleanup

--- pyfaf/hub/reports/forms.py
+++ pyfaf/hub/reports/forms.py
@@ -7,8 +7,10 @@ from pyfaf.hub.common.forms import OsComponentFilterForm
 from pyfaf.hub.common.forms import DurationOsComponentFilterForm
 
 class ReportFilterForm(OsComponentFilterForm):
-    destination = forms.ChoiceField(label="Destination", choices=[(0,"Red Hat Bugzilla"),(1,"KDE Bugtracking System")])
-    status = forms.ChoiceField(label="Status", choices=[(0,"NEW"),(1,"FIXED")])
+    destination = forms.ChoiceField(label="Destination",
+        choices=[(0,"Red Hat Bugzilla"),(1,"KDE Bugtracking System")])
+    status = forms.ChoiceField(label="Status",
+        choices=[(0,"NEW"),(1,"FIXED")])
 
     def __init__(self, db, request):
         '''
@@ -17,16 +19,19 @@ class ReportFilterForm(OsComponentFilterForm):
         super(ReportFilterForm, self).__init__(db, request)
 
         # Set initial value for destination.
-        if 'destination' in request and int(request['destination']) in (x[0] for x in self.fields['destination'].choices):
+        self.fields['destination'].initial = \
+            self.fields['destination'].choices[0][0]
+        if ('destination' in request and
+            int(request['destination']) in
+            (x[0] for x in self.fields['destination'].choices)):
             self.fields['destination'].initial = int(request['destination'])
-        else:
-            self.fields['destination'].initial = self.fields['destination'].choices[0][0]
 
         # Set initial value for status.
-        if 'status' in request and int(request['status']) in (x[0] for x in self.fields['status'].choices):
+        self.fields['status'].initial = self.fields['status'].choices[0][0]
+        if ('status' in request and
+            int(request['status']) in
+            (x[0] for x in self.fields['status'].choices)):
             self.fields['status'].initial = int(request['status'])
-        else:
-            self.fields['status'].initial = self.fields['status'].choices[0][0]
 
     def get_status_selection(self):
         return self.fields['status'].initial
@@ -35,7 +40,8 @@ class ReportFilterForm(OsComponentFilterForm):
         return self.fields['destination'].initial
 
 class ReportOverviewForm(DurationOsComponentFilterForm):
-    graph_type = forms.ChoiceField(label="Type", choices=[(0,"Absolute"),(1,"Relative")])
+    graph_type = forms.ChoiceField(label="Type",
+        choices=[(0,"Absolute"),(1,"Relative")])
 
     def __init__(self, db, request):
         '''
@@ -44,10 +50,12 @@ class ReportOverviewForm(DurationOsComponentFilterForm):
         super(ReportOverviewForm, self).__init__(db, request)
 
         # Set initial value for graph_type.
-        if 'graph_type' in request and int(request['graph_type']) in (x[0] for x in self.fields['graph_type'].choices):
+        self.fields['graph_type'].initial = \
+            self.fields['graph_type'].choices[0][0]
+        if ('graph_type' in request and
+            int(request['graph_type']) in
+            (x[0] for x in self.fields['graph_type'].choices)):
             self.fields['graph_type'].initial = int(request['graph_type'])
-        else:
-            self.fields['graph_type'].initial = self.fields['graph_type'].choices[0][0]
 
     def get_graph_type_selection(self):
         return self.fields['graph_type'].initial

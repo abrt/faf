d4f3d837231ddf0d857e4a29a6ca729c365e87d5
Michal Toman <mtoman@redhat.com>
2012-05-04 11:50:42 +0200

    webui: show backtrace in report's overview

--- pyfaf/hub/reports/urls.py
+++ pyfaf/hub/reports/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('pyfaf.hub.reports.views',
     url(r'^list/(?P<os_release>[^/]+)/(?P<component>[^/]+)/(?P<destination>[^/]+)/$', 'listing'),
     url(r'^list/(?P<os_release>[^/]+)/(?P<component>[^/]+)/(?P<destination>[^/]+)/(?P<status>[^/]+)/$', 'listing'),
 
+    url(r'^(?P<report_id>\d+)/$', 'item'),
     url(r'^$', 'index'),
     url(r'^(?P<os_release>[^/]+)/$', 'index'),
     url(r'^(?P<os_release>[^/]+)/(?P<component>[^/]+)/$', 'index'),
@@ -14,5 +15,4 @@ urlpatterns = patterns('pyfaf.hub.reports.views',
     url(r'^(?P<os_release>[^/]+)/(?P<component>[^/]+)/(?P<duration>[^/]+)/(?P<graph_type>[^/]+)/$', 'index'),
 
     url(r'^new/$', 'new'),
-    url(r'^(?P<report_id>\d+)/$', 'item'),
 )

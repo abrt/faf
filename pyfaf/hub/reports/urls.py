0351b9e666293f66d40000d337e7f6586b3f7846
Michal Toman <mtoman@redhat.com>
2012-05-11 11:14:30 +0200

    webui: match reports/new/ url correctly

--- pyfaf/hub/reports/urls.py
+++ pyfaf/hub/reports/urls.py
@@ -7,12 +7,12 @@ urlpatterns = patterns('pyfaf.hub.reports.views',
     url(r'^list/(?P<os_release>[^/]+)/(?P<component>[^/]+)/(?P<destination>[^/]+)/$', 'listing'),
     url(r'^list/(?P<os_release>[^/]+)/(?P<component>[^/]+)/(?P<destination>[^/]+)/(?P<status>[^/]+)/$', 'listing'),
 
+    url(r'^new/$', 'new'),
+
     url(r'^(?P<report_id>\d+)/$', 'item'),
     url(r'^$', 'index'),
     url(r'^(?P<os_release>[^/]+)/$', 'index'),
     url(r'^(?P<os_release>[^/]+)/(?P<component>[^/]+)/$', 'index'),
     url(r'^(?P<os_release>[^/]+)/(?P<component>[^/]+)/(?P<duration>[^/]+)/$', 'index'),
     url(r'^(?P<os_release>[^/]+)/(?P<component>[^/]+)/(?P<duration>[^/]+)/(?P<graph_type>[^/]+)/$', 'index'),
-
-    url(r'^new/$', 'new'),
 )

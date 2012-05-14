365f74b549b34861f9b9abeb11bd9d5939a9b625
Jakub Filak <jfilak@redhat.com>
2012-05-14 16:17:57 +0200

    dedup webui : correct week ticks in report graph
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- pyfaf/hub/media/report_graph.js
+++ pyfaf/hub/media/report_graph.js
@@ -5,11 +5,43 @@ function showTooltip(x, y, contents) {
     }).appendTo("body").fadeIn(200);
 }
 
-function plotRepotGraph(data, minTickSizeLabel) {
-    var graph_options = {
-       xaxis: {
-           mode: "time", minTickSize: [1, minTickSizeLabel], autoscaleMargin: 0.02,
-       },
+function plotRepotGraph(data, tickUnit) {
+   var x_axis_options = {
+     mode: "time",
+     minTickSize: [1, tickUnit],
+     autoscaleMargin: 0.02,
+   };
+
+   if ('week' == tickUnit) {
+       // FIXME - a better condition should be found
+       // this should be a half of a year
+       if (data[0].data.length < 22 ) {
+           var x_margin = 345600000   // 4 days in ms
+           var temp_data = data[0].data 
+
+           var x_min = temp_data[0][0] - x_margin;                  // first date - margin
+           var x_max = temp_data[temp_data.length-1][0] + x_margin; // last date + margin
+
+           var my_ticks = [x_min];
+           for(var i=0;i<data[0].data.length;++i) {
+               my_ticks.push(data[0].data[i][0]); // ticks are dates
+           }
+           my_ticks.push(x_max);
+
+           x_axis_options = {
+               min: x_min,
+               max: x_max,
+               ticks: my_ticks,
+               mode: "time",
+           };
+       }
+       else {
+           x_axis_options.minTickSize[1]='day';
+       }
+   }
+                
+   var graph_options = {
+       xaxis: x_axis_options,
        series: {
            points: {show: true},
            lines: {show: true},

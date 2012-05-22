563cae68b5c76c80f961d7dba6e3b162e7989191
Richard Marko <rmarko@redhat.com>
2012-05-22 21:01:41 +0200

    dedup webui: whitespace cleanup

--- pyfaf/hub/media/report_graph.js
+++ pyfaf/hub/media/report_graph.js
@@ -17,7 +17,7 @@ function plotRepotGraph(data, tickUnit) {
        // this should be a half of a year
        if (data[0].data.length < 22 ) {
            var x_margin = 345600000   // 4 days in ms
-           var temp_data = data[0].data 
+           var temp_data = data[0].data
 
            var x_min = temp_data[0][0] - x_margin;                  // first date - margin
            var x_max = temp_data[temp_data.length-1][0] + x_margin; // last date + margin
@@ -39,7 +39,7 @@ function plotRepotGraph(data, tickUnit) {
            x_axis_options.minTickSize[1]='day';
        }
    }
-                
+
    var graph_options = {
        xaxis: x_axis_options,
        series: {

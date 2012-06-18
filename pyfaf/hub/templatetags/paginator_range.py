d3edd67838c141d1d60093d74535f166203e0520
Richard Marko <rmarko@redhat.com>
2012-06-18 16:06:57 +0200

    hub: rewrite page range tag to use standard tag
    Due to Django < 1.4 compatibility reasons - assignment_tag is
    not supported in older versions of Django.
    
    This can be reverted when we stop supporting Django 1.3.

--- pyfaf/hub/templatetags/paginator_range.py
+++ pyfaf/hub/templatetags/paginator_range.py
@@ -2,15 +2,36 @@ from django import template
 
 register = template.Library()
 
-@register.assignment_tag
-def get_page_range(current, num_pages, adjacent_pages=2):
-    start = max(current - adjacent_pages, 1)
-    if start <= 3:
-         start = 1
+class PageRangeNode(template.Node):
+    def __init__(self, current_var, num_pages_var, adjacent_var):
+        self.current_var = current_var
+        self.adjacent_var = adjacent_var
+        self.num_pages_var = num_pages_var
 
-    end = current + adjacent_pages + 1
-    if end > num_pages:
-        end = num_pages + 1
+    def render(self, context):
+        current = self.current_var.resolve(context)
+        adjacent_pages = self.adjacent_var.resolve(context)
+        num_pages = self.num_pages_var.resolve(context)
 
-    page_numbers = [n for n in range(start, end)]
-    return page_numbers
+        start = max(current - adjacent_pages, 1)
+        if start <= 3:
+             start = 1
+
+        end = current + adjacent_pages + 1
+        if end > num_pages:
+            end = num_pages + 1
+
+        page_numbers = [n for n in range(start, end)]
+
+        context['range'] = page_numbers
+        return ''
+
+@register.tag
+def get_page_range(parser, token):
+    bits = token.split_contents()[1:]
+
+    if len(bits) != 3:
+        raise template.TemplateSyntaxError(
+            "'get_page_range' tag takes exactly 3 arguments")
+
+    return PageRangeNode(*map(template.Variable, bits))

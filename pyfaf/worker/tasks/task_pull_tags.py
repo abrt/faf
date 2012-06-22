d7b54dec7fad6160a1f04ccf9c6f0b1d9b18533c
Michal Toman <mtoman@redhat.com>
2012-06-22 15:48:01 +0200

    add kobo task/cmd for pull-tags

---
+++ pyfaf/worker/tasks/task_pull_tags.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from kobo.worker import TaskBase
+from kobo.worker import FailTaskException
+from subprocess import Popen, PIPE, STDOUT
+
+class PullTags(TaskBase):
+    enabled = True
+
+    arches = ["noarch"]    # list of supported architectures
+    channels = ["default"] # list of channels
+    exclusive = False      # leave False here unless you really know what you're doing
+    foreground = True      # if True the task is not forked and runs in the worker process (no matter you run worker without -f)
+    priority = 19
+    weight = 1.0
+
+    def run(self):
+        if len(self.args) != 1:
+            self.result = "Exactly one argument expected"
+            raise FailTaskException
+
+        child = Popen(["faf-koji-pull-tags", "-v", self.args["os"]], stdout=PIPE, stderr=STDOUT)
+        line = child.stdout.readline()
+        while line:
+            sys.stdout.write(line)
+            sys.stdout.flush()
+            line = child.stdout.readline()
+
+        retcode = child.wait()
+        child.stdout.close()
+        if retcode:
+            self.result = "faf-koji-pull-tags exitted with {0}".format(retcode)
+            raise FailTaskException
+
+        self.result = "Tags updated successfully"
+
+    @classmethod
+    def cleanup(cls, hub, conf, task_info):
+        pass
+
+    @classmethod
+    def notification(cls, hub, conf, task_info):
+        pass
+        # hub.worker.email_<foo>_notification(task_info["id"])

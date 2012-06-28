26491368b3ec75931059a7687c9ffee569d62a52
Richard Marko <rmarko@redhat.com>
2012-06-28 16:40:15 +0200

    hub: basic tests for reports overview

---
+++ pyfaf/hub/reports/tests/__init__.py
@@ -0,0 +1,4 @@
+from pyfaf.hub.reports.tests.overview import (ReportsSummaryTest,
+                                              ReportsGraphTest)
+
+__all__ = [ReportsSummaryTest, ReportsGraphTest]

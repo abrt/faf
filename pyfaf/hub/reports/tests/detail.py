bfc84ef3546ce1e3c21753958ca4eb3eedadbf31
Richard Marko <rmarko@redhat.com>
2012-07-02 18:06:24 +0200

    hub: access report via link on lists page

--- pyfaf/hub/reports/tests/detail.py
+++ pyfaf/hub/reports/tests/detail.py
@@ -2,8 +2,13 @@ from pyfaf.hub.common.tests import SeleniumTest
 
 class ReportDetailTest(SeleniumTest):
     def test_title(self):
-        self.load('/reports/%d/' % 50)
-        self.assertIn('Report #50', self.title)
+        self.load('/reports/list/')
+        reports = self.find('article table tr')
+        report = reports[10]
+        link = self.find('td a', report)
+        target = link.text
+        self.click(link)
+        self.assertIn('Report #%s' % target, self.title)
 
     def test_menu_entries(self):
         self.load('/reports/%d/' % 50)

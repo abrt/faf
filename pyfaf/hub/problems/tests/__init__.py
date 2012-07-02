edb75a50ea68cda9e24a84a9d80a0d389cbf6a1a
Richard Marko <rmarko@redhat.com>
2012-07-02 17:50:02 +0200

    hub: basic tests for hot problems page

---
+++ pyfaf/hub/problems/tests/__init__.py
@@ -0,0 +1,3 @@
+from pyfaf.hub.problems.tests.hot import HotProblemsTest
+
+__all__ = [HotProblemsTest]
---
+++ pyfaf/hub/problems/tests/hot.py
@@ -0,0 +1,12 @@
+from pyfaf.hub.common.tests import SeleniumTest
+
+class HotProblemsTest(SeleniumTest):
+    def test_title(self):
+        self.load('/problems/hot/')
+        self.assertIn('Hot problems', self.title)
+
+    def test_menu_entries(self):
+        self.load('/problems/hot/')
+        menu_entries = self.find('nav li.active a')
+        self.assertEqual(['Problems', 'Hot Problems'],
+            map(lambda x: x.text, menu_entries))

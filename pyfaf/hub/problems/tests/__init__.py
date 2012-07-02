b3efad6d2596542300553058e2b87d553c094390
Richard Marko <rmarko@redhat.com>
2012-07-02 17:52:20 +0200

    hub: basic tests for longterm problems page

--- pyfaf/hub/problems/tests/__init__.py
+++ pyfaf/hub/problems/tests/__init__.py
@@ -1,3 +1,4 @@
 from pyfaf.hub.problems.tests.hot import HotProblemsTest
+from pyfaf.hub.problems.tests.longterm import LongtermProblemsTest
 
-__all__ = [HotProblemsTest]
+__all__ = [HotProblemsTest, LongtermProblemsTest]

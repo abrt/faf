9786ca0a5eb5de08e5acd5cce6e7a51e9ae744bf
Richard Marko <rmarko@redhat.com>
2012-09-18 15:44:55 +0200

    hub: shift summary graph to the left
    More padding added to the right side so it doesn't
    overlap with graph legend.
    
    Handling unified over different time scales.

--- pyfaf/hub/media/report_graph.js
+++ pyfaf/hub/media/report_graph.js
@@ -6,78 +6,88 @@ function showTooltip(x, y, contents) {
 }
 
 function plotReportGraph(data, tick_unit) {
-   var x_axis_options = {
-     mode: "time",
-     minTickSize: [1, tick_unit],
-     autoscaleMargin: 0.02,
-   };
+    var msday = 86400000; // one day in ms
+    var temp_data = data[0].data;
+    var x_min = temp_data[0][0];
+    var x_max = temp_data[temp_data.length-1][0];
+    var x_axis_options = {}
 
-   if (tick_unit == 'week') {
-       // FIXME - a better condition should be found
-       // this should be a half of a year
-       if (data[0].data.length < 22 ) {
-           var x_margin = 345600000   // 4 days in ms
-           var temp_data = data[0].data
+    if (tick_unit == 'week') {
+        var x_margin = msday * 7;
+        var my_ticks = [x_min - x_margin];
 
-           var x_min = temp_data[0][0] - x_margin;                  // first date - margin
-           var x_max = temp_data[temp_data.length-1][0] + x_margin; // last date + margin
+        for(var i=0; i<data[0].data.length; ++i) {
+            my_ticks.push(data[0].data[i][0]); // ticks are dates
+        }
 
-           var my_ticks = [x_min];
-           for(var i=0;i<data[0].data.length;++i) {
-               my_ticks.push(data[0].data[i][0]); // ticks are dates
-           }
-           my_ticks.push(x_max);
+        my_ticks.push(x_max + x_margin);
+        my_ticks.push(x_max + x_margin*2);
 
-           x_axis_options = {
-               min: x_min,
-               max: x_max,
-               ticks: my_ticks,
-               mode: "time",
-           };
-       }
-       else {
-           x_axis_options.minTickSize[1]='day';
-       }
-   }
+        x_axis_options = {
+            min: x_min - x_margin,
+            max: x_max + x_margin*2,
+            ticks: my_ticks,
+            mode: "time",
+        };
+    } else if (tick_unit == 'day') {
+        x_axis_options = {
+            min: x_min - msday,
+            max: x_max + msday*2,
+            ticks: 20,
+            mode: "time",
+        };
+    } else if (tick_unit == 'month') {
+        x_axis_options = {
+            min: x_min - msday*30,
+            max: x_max + msday*60,
+            ticks: 12,
+            mode: "time",
+        };
+    } else {
+        x_axis_options = {
+            mode: "time",
+            ticks: 12,
+            autoscaleMargin: 0.1,
+        };
+    }
 
-   var graph_options = {
-       xaxis: x_axis_options,
-       yaxis: {
-           min: 0,
-           tickDecimals: 0,
-       },
-       series: {
-           points: {show: true},
-           lines: {show: true},
-       },
-       grid: {
-           hoverable: true,
-           clickable: true,
-           borderColor: '#aaa',
-           borderWidth: 1,
-       },
+    var graph_options = {
+        xaxis: x_axis_options,
+        yaxis: {
+            min: 0,
+            tickDecimals: 0,
+        },
+        series: {
+            points: {show: true},
+            lines: {show: true},
+        },
+        grid: {
+            hoverable: true,
+            clickable: true,
+            borderColor: '#aaa',
+            borderWidth: 1,
+        },
     };
 
     var data_config = [];
-    for(var i=0;i<data.length;++i) {
+    for(var i=0; i<data.length; ++i) {
         data_config.push( {data: data[i].data, label: data[i].label} );
     }
 
     var previousPoint = null;
     $.plot($("#placeholder"), data_config, graph_options);
     $("#placeholder").bind("plothover", function (event, pos, item) {
-    if (item) {
-        if (previousPoint != item.dataIndex) {
-            previousPoint = item.dataIndex;
+        if (item) {
+            if (previousPoint != item.dataIndex) {
+                previousPoint = item.dataIndex;
+                $("#tooltip").remove();
+                var x = item.datapoint[0].toFixed(2),
+                    y = item.datapoint[1].toFixed(2);
+                showTooltip(item.pageX, item.pageY, parseInt(y) + " reports");
+            }
+        } else {
             $("#tooltip").remove();
-            var x = item.datapoint[0].toFixed(2),
-                y = item.datapoint[1].toFixed(2);
-            showTooltip(item.pageX, item.pageY,
-                        parseInt(y) + " reports");
+            previousPoint = null;
         }
-      } else {
-          $("#tooltip").remove();
-          previousPoint = null;
-      }
     });
 }

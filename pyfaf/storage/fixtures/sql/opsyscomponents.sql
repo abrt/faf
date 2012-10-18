5c34ff486fa108fe2ea2cc47688c0b618f31dc85
Richard Marko <rmarko@redhat.com>
2012-10-18 17:16:45 +0200

    fixtures: add kernel component

--- pyfaf/storage/fixtures/sql/opsyscomponents.sql
+++ pyfaf/storage/fixtures/sql/opsyscomponents.sql
@@ -1,7 +1,9 @@
 -- Fedora
 INSERT INTO opsyscomponents VALUES (1, 'will-crash', 1);
 INSERT INTO opsyscomponents VALUES (2, 'glibc', 1);
+INSERT INTO opsyscomponents VALUES (3, 'kernel', 1);
 
 -- EL
-INSERT INTO opsyscomponents VALUES (3, 'will-crash', 2);
-INSERT INTO opsyscomponents VALUES (4, 'glibc', 2);
+INSERT INTO opsyscomponents VALUES (4, 'will-crash', 2);
+INSERT INTO opsyscomponents VALUES (5, 'glibc', 2);
+INSERT INTO opsyscomponents VALUES (6, 'kernel', 2);
--- pyfaf/storage/fixtures/sql/opsysreleasescomponents.sql
+++ pyfaf/storage/fixtures/sql/opsysreleasescomponents.sql
@@ -1,7 +1,9 @@
--- will-crash & glibc for F17
+-- will-crash, glibc, kernel for F17
 INSERT INTO opsysreleasescomponents VALUES (1, 3, 1);
 INSERT INTO opsysreleasescomponents VALUES (2, 3, 2);
+INSERT INTO opsysreleasescomponents VALUES (3, 3, 3);
 
--- will-crash & glibc for EL7
-INSERT INTO opsysreleasescomponents VALUES (3, 5, 3);
+-- will-crash, glibc, kernel for EL7
 INSERT INTO opsysreleasescomponents VALUES (4, 5, 4);
+INSERT INTO opsysreleasescomponents VALUES (5, 5, 5);
+INSERT INTO opsysreleasescomponents VALUES (6, 5, 6);

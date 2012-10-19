ea3cce7c91831cc918f234152622aac160ebdbc7
Richard Marko <rmarko@redhat.com>
2012-10-19 18:57:27 +0200

    hub: keep filter when switching hot and long-term problems
    trac#784

--- pyfaf/hub/media/form.js
+++ pyfaf/hub/media/form.js
@@ -20,4 +20,20 @@ $(document).ready(function() {
 
     var sel = $('select[multiple] option:selected[value="*"]').removeAttr('selected');
     $('select[multiple]').select2({placeholder: sel.text()});
+
+    // if filter is enabled, update both Hot & Longterm problem links to preserve it
+    var match;
+    if(match = window.location.pathname.match('/problems/(hot|longterm)/(.*)')) {
+        // find button with href ending like this
+        var second_button=$('a[href$="/problems/'+
+            (match[1] == 'hot' ? 'longterm' : 'hot') +'/"]');
+
+        // skip top nav button
+        if(second_button.length >= 2)
+            second_button = $(second_button[1]);
+
+        // append filter string
+        second_button.attr('href',
+            second_button.attr('href') + match[2]);
+    }
 });

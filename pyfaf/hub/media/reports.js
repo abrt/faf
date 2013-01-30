64d90e5cc62ab22ac22f44236942f74395439e1a
Richard Marko <rmarko@redhat.com>
2013-01-30 17:25:46 +0100

    webui: expose diff functionality

---
+++ pyfaf/hub/media/reports.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  var ctab = $('.tab-content .tab-pane:first-child').attr('id');
+  var ptab = ctab;
+  var reports_url = $('#btn-view-report').attr('href').replace(/\d+\/$/g, '');
+  var diff_url =  $('#btn-diff-reports').attr('href').replace(/\d+\/\d+\/$/g, '');
+  var a = ctab;
+  var b = ctab;
+
+  /* Update 'View complete report' button URL and diff selection on tab change */
+  $('a[data-toggle="tab"]').on('shown', function(e) {
+    ctab = e.target.text;
+    ptab = e.relatedTarget.text;
+    $('#btn-view-report').attr('href', reports_url + ctab + '/');
+    $('#select-b').val(ctab).change();
+  });
+
+  /* Show diff dropdowns and pre-select reports in current & previous tab */
+  $('#btn-diff').click(function() {
+    $('#btn-diff').hide();
+    $('#select-a').val(ctab).change();
+    $('#select-b').val(ptab).change();
+    $('#diff-choice').css('display', 'inline');
+  });
+
+  /* Update 'Diff' button URL on changes of diff selects */
+  $('#diff-choice select').change(function() {
+    var selected = $(this).find('option:selected').attr('value')
+    if($(this).attr('name') == 'select-a') {
+      a = selected;
+    } else {
+      b = selected;
+    }
+
+    $('#btn-diff-reports').attr('href', diff_url + a + '/' + b + '/');
+  });
+
+  /* Handle browser history */
+  $('#diff-choice select').change();
+});

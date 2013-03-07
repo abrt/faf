7d614e4a57901dd686b50b3a6d71a11c98e668a4
Richard Marko <rmarko@redhat.com>
2013-03-07 14:28:51 +0100

    hub: remove + N more button instead of hiding it

--- pyfaf/hub/media/metrics.js
+++ pyfaf/hub/media/metrics.js
@@ -1,7 +1,7 @@
 metrics_more_buttons = function() {
   $('.btn-more').click(function() {
-    $(this).parents('tr').hide();
     $(this).parents('table').find('tr.hide').show();
+    $(this).parents('tr').remove();
   });
 }
 

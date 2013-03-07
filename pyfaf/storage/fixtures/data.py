45a10506158e8e36956881dcf27875b3c08c8857
Richard Marko <rmarko@redhat.com>
2013-03-07 11:40:34 +0100

    fixtures: update source data

--- pyfaf/storage/fixtures/data.py
+++ pyfaf/storage/fixtures/data.py
@@ -1,4 +1,3 @@
-import sys
 from datetime import date
 
 ARCH = [
@@ -14,22 +13,21 @@ ARCH = [
     ]
 
 OPSYS = {
-    'Fedora':   [('15',    date(2011, 5, 24)),
-                 ('16',    date(2011, 11, 8)),
-                 ('17',    date(2012, 5, 22)),
+    'Fedora':   [('17',    date(2012, 5, 22)),
+                 ('18',    date(2013, 1, 15)),
                  ('devel', None)],
 
     'RHEL':     [('6',   date(2010, 11, 10)),
                  ('6.1', date(2011, 5, 9)),
                  ('6.2', date(2011, 12, 6)),
-                 ('6.3', None),
+                 ('6.3', date(2012, 6, 20)),
                  ('7',   None)],
 
     'openSUSE': [('11.4', date(2011, 3, 10)),
                  ('12.1', date(2011, 11, 16))]
     }
 
-COMPS = set(map(lambda x: x.replace('_', '').split('.')[0], sys.modules))
+COMPS = ['abrt', 'libreport', 'btparser', 'will-crash']
 
 _LIBS = ['gtk', 'gdk', 'dbus', 'xul', 'GL', 'jvm', 'freetype']
 LIBS = map(lambda x: 'lib%s' % x, _LIBS)

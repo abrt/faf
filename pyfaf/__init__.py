d86850b8d2b676011dd7747d44e6c28f7da67af1
Richard Marko <rmarko@redhat.com>
2013-03-12 14:30:44 +0100

    set DJANGO_SETTINGS_MODULE automatically in pyfaf init

--- pyfaf/__init__.py
+++ pyfaf/__init__.py
@@ -12,6 +12,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyfaf.hub.settings')
+
 from . import config
 from . import terminal
 from . import argparse

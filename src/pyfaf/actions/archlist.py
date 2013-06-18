96b587767379e833236b60531908dc098318a473
Richard Marko <rmarko@redhat.com>
2013-06-18 12:05:15 +0200

    add archlist action
    Signed-off-by: Richard Marko <rmarko@redhat.com>

---
+++ src/pyfaf/actions/archlist.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2013  ABRT Team
+# Copyright (C) 2013  Red Hat, Inc.
+#
+# This file is part of faf.
+#
+# faf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# faf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with faf.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyfaf.actions import Action
+from pyfaf.storage.opsys import Arch
+
+
+class ArchList(Action):
+    name = "archlist"
+
+    def __init__(self):
+        super(ArchList, self).__init__()
+
+    def run(self, cmdline, db):
+        for item in db.session.query(Arch):
+            print(item)

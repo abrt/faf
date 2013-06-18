23569faea8498a85f5173d47c6ba5e92216348c1
Richard Marko <rmarko@redhat.com>
2013-06-18 12:30:45 +0200

    add repodel action
    Signed-off-by: Richard Marko <rmarko@redhat.com>

---
+++ src/pyfaf/actions/repodel.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2013  ABRT Team
+# Copyright (C) 2013  Red Hat, Inc.
+#
+# This file is part of faf.
+#
+# faf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# faf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with faf.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyfaf.actions import Action
+from pyfaf.storage.opsys import Repo
+
+
+class RepoDel(Action):
+    name = "repodel"
+
+    def __init__(self):
+        super(RepoDel, self).__init__()
+
+    def run(self, cmdline, db):
+        repo = (db.session.query(Repo)
+                          .filter(Repo.name == cmdline.NAME)
+                          .first())
+
+        if not repo:
+            self.log_error("Repository '{0}' not found"
+                           .format(cmdline.NAME))
+            return 1
+
+        self.log_info("Removing repository '{0}'".format(cmdline.NAME))
+
+        db.session.delete(repo)
+        db.session.flush()
+
+    def tweak_cmdline_parser(self, parser):
+        parser.add_argument("NAME", help="name of the repository to delete")

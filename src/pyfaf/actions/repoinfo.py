4ba9427a7598d8999a8ff0a5cc81bf1f3a6cfaf4
Richard Marko <rmarko@redhat.com>
2013-06-18 17:04:06 +0200

    add repoinfo action
    Signed-off-by: Richard Marko <rmarko@redhat.com>

---
+++ src/pyfaf/actions/repoinfo.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2013  ABRT Team
+# Copyright (C) 2013  Red Hat, Inc.
+#
+# This file is part of faf.
+#
+# faf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# faf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with faf.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyfaf.actions import Action
+from pyfaf.storage.opsys import Repo
+
+
+class RepoInfo(Action):
+    name = "repoinfo"
+
+    def __init__(self):
+        super(RepoInfo, self).__init__()
+
+    def run(self, cmdline, db):
+        repo = (db.session.query(Repo)
+                          .filter(Repo.name == cmdline.NAME)
+                          .first())
+
+        if not repo:
+            self.log_error("Repository '{0}' not found".format(cmdline.NAME))
+            return 1
+
+        print("Name: {0}".format(repo.name))
+
+        if repo.nice_name:
+            print("Nice name: {0}".format(repo.nice_name))
+
+        print("Type: {0}".format(repo.type))
+        print("URL: {0}".format(repo.url))
+        print("GPG check enabled: {0}".format(not repo.nogpgcheck))
+
+        if cmdline.assigned:
+            if repo.opsys_list:
+                print("Operating systems: {0}"
+                      .format(", ".join(map(str, repo.opsys_list))))
+
+            if repo.arch_list:
+                print("Architectures: {0}"
+                      .format(", ".join(map(str, repo.arch_list))))
+
+    def tweak_cmdline_parser(self, parser):
+        parser.add_argument("NAME", help="name of the repository")
+        parser.add_argument("-a", "--assigned", action="store_true",
+                            help="list assigned operating systems"
+                                 " and architectures")

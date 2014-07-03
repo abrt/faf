908b71a8bca3183dfe171f6158fb23959f2dc9a6
Michal Toman <mtoman@redhat.com>
2014-07-03 11:31:04 +0200

    koops: do not reject koopses without modules; closes #241
    Signed-off-by: Michal Toman <mtoman@redhat.com>

--- src/pyfaf/problemtypes/kerneloops.py
+++ src/pyfaf/problemtypes/kerneloops.py
@@ -91,7 +91,8 @@ class KerneloopsProblem(ProblemType):
 
         "taint_flags": ListChecker(StringChecker(allowed=tainted_flags.keys())),
 
-        "modules":     ListChecker(StringChecker(pattern=r"^[a-zA-Z0-9_]+(\([A-Z\+\-]+\))?$")),
+        "modules":     ListChecker(StringChecker(pattern=r"^[a-zA-Z0-9_]+(\([A-Z\+\-]+\))?$"),
+                                   mandatory=False),
 
         "raw_oops": StringChecker(maxlen=Report.__lobs__["oops"],
                                   mandatory=False),

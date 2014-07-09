588879f485f38c4a274ec272ccc025693b270e06
Michal Toman <mtoman@redhat.com>
2014-07-09 16:45:15 +0200

    java: do not die when crashfn is None
    Signed-off-by: Michal Toman <mtoman@redhat.com>

--- src/pyfaf/problemtypes/java.py
+++ src/pyfaf/problemtypes/java.py
@@ -228,7 +228,7 @@ class JavaProblem(ProblemType):
                 crashfn = frame["name"]
                 break
 
-        if "." in crashfn:
+        if crashfn is not None and "." in crashfn:
             crashfn = crashfn.rsplit(".", 1)[1]
 
         errname = None

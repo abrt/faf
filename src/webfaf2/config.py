bc3234242f556a40fd7baf481fe9fdbb0069fcba
Richard Marko <rmarko@fedoraproject.org>
2014-09-17 15:38:27 +0200

    webfaf2: rework production config
    Signed-off-by: Richard Marko <rmarko@fedoraproject.org>

--- src/webfaf2/config.py
+++ src/webfaf2/config.py
@@ -1,17 +1,19 @@
 from sqlalchemy.engine.url import _parse_rfc1738_args
 from pyfaf.config import config
+from pyfaf.utils.parse import str2bool
 dburl = _parse_rfc1738_args(config["storage.connectstring"])
 
 
 class Config(object):
     DEBUG = False
     TESTING = False
-    SECRET_KEY = '@RANDOM_STRING@'
+    SECRET_KEY = 'NOT_A_RANDOM_STRING'
     SQLALCHEMY_DATABASE_URI = dburl
 
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = str2bool(config["hub2.debug"])
+    SECRET_KEY = config["hub2.secret_key"]
 
 
 class DevelopmentConfig(Config):

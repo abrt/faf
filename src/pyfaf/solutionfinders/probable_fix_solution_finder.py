288bf40dbf3db7a90db7fff17179471cb9364da2
Marek Bryša <mbrysa@redhat.com>
2014-12-09 09:54:11 +0100

    pyfaf: don't return a solution if affected package is newer than prob. fix
    Closes #316

--- src/pyfaf/solutionfinders/probable_fix_solution_finder.py
+++ src/pyfaf/solutionfinders/probable_fix_solution_finder.py
@@ -16,19 +16,59 @@
 # You should have received a copy of the GNU General Public License
 # along with faf.  If not, see <http://www.gnu.org/licenses/>.
 
-import re
 from pyfaf.solutionfinders import SolutionFinder, Solution
-from pyfaf.common import log
-from pyfaf.opsys import systems
-from pyfaf.problemtypes import problemtypes
-from pyfaf.queries import (get_sf_prefilter_btpaths, get_sf_prefilter_pkgnames,
-                           get_opsys_by_name)
-from pyfaf.ureport_compat import ureport1to2
+from pyfaf.ureport import ureport2
+from pyfaf.utils.parse import cmp_evr
 
 
 class ProbableFixSolutionFinder(SolutionFinder):
     name = "sf-probable-fix"
     nice_name = "Probable Fix Solution"
+
+    def _posr_to_solution(self, posr):
+        text = ("A new package version is available in which the "
+                "problem has not yet occured. Please update to "
+                "the following version and delete the ABRT problem:\n{}"
+                .format(posr.probable_fix))
+        html = ("A new package version is available in which the "
+                "problem has not yet occured. Please consider "
+                "updating to at least the following version:"
+                "<br/><pre>{}</pre>"
+                .format(posr.probable_fix))
+        since = None
+        if posr.probably_fixed_since:
+            since = posr.probably_fixed_since
+        return Solution(cause="Outdated package",
+                        url="",
+                        note_text=text,
+                        note_html=html,
+                        since=since)
+
+    def find_solution_ureport(self, db, ureport, osr=None):
+        ureport = ureport2(ureport)
+        db_report = self._get_db_report(db, ureport)
+        if db_report is None:
+            return None
+
+        if db_report.problem is None:
+            return None
+
+        for posr in db_report.problem.opsysreleases:
+            if osr is None or posr.opsysrelease_id == osr.id:
+                if posr.probable_fix_build is not None:
+                    db_build = posr.probable_fix_build
+                    for pkg in ureport["packages"]:
+                        if pkg.get("package_role", "") == "affected":
+                            break
+                    if pkg.get("package_role", "") != "affected":
+                        return None
+                    # Fixing version must be greater than affected version
+                    if cmp_evr((pkg["epoch"], pkg["version"], pkg["release"]),
+                               (db_build.epoch, db_build.version, db_build.release)) < 0:
+                        return self._posr_to_solution(posr)
+                    else:
+                        return None
+        return None
 
     def find_solution_db_report(self, db, db_report, osr):
         if db_report is None:
@@ -40,22 +80,6 @@ class ProbableFixSolutionFinder(SolutionFinder):
         for posr in db_report.problem.opsysreleases:
             if osr is None or posr.opsysrelease_id == osr.id:
                 if posr.probable_fix:
-                    text = ("A new package version is available in which the "
-                            "problem has not yet occured. Please update to "
-                            "the following version and delete the ABRT problem:\n{}"
-                            .format(posr.probable_fix))
-                    html = ("A new package version is available in which the "
-                            "problem has not yet occured. Please consider "
-                            "updating to at least the following version:"
-                            "<br/><pre>{}</pre>"
-                            .format(posr.probable_fix))
-                    since = None
-                    if posr.probably_fixed_since:
-                        since = posr.probably_fixed_since
-                    return Solution(cause="Outdated package",
-                                    url="",
-                                    note_text=text,
-                                    note_html=html,
-                                    since=since)
+                    return self._posr_to_solution(posr)
 
         return None

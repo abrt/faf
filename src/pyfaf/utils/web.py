e437d49cc5bc12639a0b7592badfe7d23bc03af7
Matej Habrnal <mhabrnal@redhat.com>
2017-04-19 13:34:00 +0200

    Fix url to FAF report in FedMsg notification
    URLs attached into FedMsg notifications were always prefixed
    by 'http://' even the server runs over https.
    The URL prefix is now controlled by a new option 'require_https' which
    was added into 'web.conf'. If the option is 'true' URLs are prefixed
    by 'https://'.
    
    Fixes #564
    
    Signed-off-by: Matej Habrnal <mhabrnal@redhat.com>

--- src/pyfaf/utils/web.py
+++ src/pyfaf/utils/web.py
@@ -39,6 +39,19 @@ def server_name():
         return None
 
 
+def require_https():
+    """
+    Return if web server requires https (default true)
+    """
+
+    if webfaf_installed():
+        from pyfaf.utils.parse import str2bool
+        return str2bool(config.get("hub.require_https", "true"))
+    else:
+        logging.warn("Unable to get require https option, webfaf not available")
+        return True
+
+
 def reverse(view, **kwargs):
     """
     Return full URL to pointing to `view`
@@ -49,6 +62,7 @@ def reverse(view, **kwargs):
         from flask import url_for
         from webfaf.webfaf_main import app
         app.config["SERVER_NAME"] = server_name()
+        app.config['PREFERRED_URL_SCHEME'] = 'https' if require_https() else 'http'
         with app.app_context():
             kwargs["_external"] = True
             return url_for(view, **kwargs)

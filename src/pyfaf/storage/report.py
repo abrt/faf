0d9794febda885e5283a3c35efe875e77d3d452f
Rostislav Horák <warchief147@gmail.com>
2017-05-15 15:49:26 +0200

    :160, 0: Wrong continued indentation before block (add 4 spaces).
    C:161, 0: Wrong continued indentation before block (add 4 spaces).
    
    C: 38, 0: Wrong continued indentation (add 1 space).
    
    C:205, 0: Wrong continued indentation before block (add 4 spaces).
    
    W:230, 0: Bad indentation. Found 20 spaces, expected 16 (bad-indentation)

--- src/pyfaf/storage/report.py
+++ src/pyfaf/storage/report.py
@@ -202,7 +202,7 @@ class ReportBacktrace(GenericTable):
             frame_t.source_path = None
             frame_t.line_num = None
             if (frame.symbolsource.source_path and
-                frame.symbolsource.line_number):
+                    frame.symbolsource.line_number):
 
                 frame_t.source_path = frame.symbolsource.source_path
                 frame_t.line_num = frame.symbolsource.line_number
@@ -227,7 +227,7 @@ class ReportBacktrace(GenericTable):
             if not frame.symbolsource.symbol:
                 quality -= 1
             elif frame.symbolsource.symbol.name == '??':
-                    quality -= 1
+                quality -= 1
 
             if not frame.symbolsource.source_path:
                 quality -= 1

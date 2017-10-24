1a54d6a86b51cb240c158b2d264018005198598d
Matej Marusak <mmarusak@redhat.com>
2017-10-24 12:48:53 +0200

    config: Enable setting up DB with env variables
    Signed-off-by: Matej Marusak <mmarusak@redhat.com>

--- src/pyfaf/common.py
+++ src/pyfaf/common.py
@@ -225,11 +225,20 @@ def get_temp_dir(subdir=None):
     return os.path.join(basetmp, userdir, subdir)
 
 
+def _get_env_or_config(conf_s, env_s, default):
+    found = os.environ.get(env_s, None)
+    if not found:
+        found = config.get(conf_s, None)
+        if not found:
+            found = default
+    return found
+
+
 def get_connect_string():
     """Create connection string for database from config file."""
     login = ""
-    user = config.get("storage.dbuser", "")
-    passwd = config.get("storage.dbpasswd", "")
+    user = _get_env_or_config("storage.dbuser", "PGUSER", "")
+    passwd = _get_env_or_config("storage.dbpasswd", "PGPASSWORD", "")
     # password without user does not make sense
     if user:
         if passwd:
@@ -237,8 +246,8 @@ def get_connect_string():
         else:
             login = user
 
-    host = config.get("storage.dbhost", "")
-    port = config.get("storage.dbport", "")
+    host = _get_env_or_config("storage.dbhost", "PGHOST", "")
+    port = _get_env_or_config("storage.dbport", "PGPORT", "")
     # port without host does not make sense
     if host:
         if port:
@@ -246,7 +255,7 @@ def get_connect_string():
         else:
             login = login + "@" + host
 
-    return "postgresql://" + login + "/" + config.get("storage.dbname", "")
+    return "postgresql://" + login + "/" + _get_env_or_config("storage.dbname", "PGDATABASE", "")
 
 
 class FafError(Exception):

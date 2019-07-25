abrt/faf

commit e40e33e177624c830a816df05b3aae8ec8bddf22

py3 compatibility: Encode unicode-objects
Signed-off-by: Jan Beran <jberan@redhat.com>

src/pyfaf/problemtypes/python.py
@@ -19,6 +19,7 @@
 from __future__ import unicode_literals
 from string import ascii_uppercase
 import satyr
+import sys
 
 from pyfaf.problemtypes import ProblemType
 from pyfaf.checker import (CheckError,
@@ -144,8 +145,11 @@ class PythonProblem(ProblemType):
                 frame.function_name = funcname
             frame.file_line = db_frame.symbolsource.offset
             frame.file_name = db_frame.symbolsource.path
-            if db_frame.symbolsource.srcline is not None:
-                frame.line_contents = db_frame.symbolsource.srcline.encode("utf-8")
+            if sys.version_info.major == 2:
+                if db_frame.symbolsource.srcline is not None:
+                    frame.line_contents = db_frame.symbolsource.srcline.encode("utf-8")
+                else:
+                    frame.line_contents = db_frame.symbolsource.srcline
 
             stacktrace.frames.append(frame)
 

src/pyfaf/actions/addcompathashes.py
@@ -74,7 +74,7 @@ class AddCompatHashes(Action):
         else:
             raise FafError("either function names or hashes are required")
 
-        return hashlib.sha1("\n".join(hashbase)).hexdigest()
+        return hashlib.sha1("\n".join(hashbase).encode("utf-8")).hexdigest()
 
     def run(self, cmdline, db):
         if cmdline.problemtype is None or len(cmdline.problemtype) < 1:

src/pyfaf/actions/save_reports.py
@@ -255,7 +255,7 @@ class SaveReports(Action):
                     h = hashlib.sha1()
                     h.update(contents)
                     h.update(datetime.date.fromtimestamp(stat.st_mtime)
-                             .isoformat())
+                             .isoformat().encode("utf-8"))
                     digest = h.digest()
                     if digest in reports:
                         reports[digest]["filenames"].append(fname)

src/pyfaf/problemtypes/core.py
@@ -21,6 +21,7 @@ from __future__ import unicode_literals
 import os
 import shutil
 import satyr
+import sys
 from pyfaf.problemtypes import ProblemType
 from pyfaf.checker import (Checker,
                            DictChecker,
@@ -175,8 +176,12 @@ class CoredumpProblem(ProblemType):
         for db_frame in db_thread.frames:
             frame = satyr.GdbFrame()
             frame.address = db_frame.symbolsource.offset
-            frame.library_name = \
-                db_frame.symbolsource.path.encode("ascii", "ignore")
+            if sys.version_info.major == 2:
+                frame.library_name = \
+                    db_frame.symbolsource.path.encode("ascii", "ignore")
+            else:
+                frame.library_name = \
+                    db_frame.symbolsource.path
             frame.number = db_frame.order
             if db_frame.symbolsource.symbol is not None:
                 frame.function_name = db_frame.symbolsource.symbol.name
@@ -184,8 +189,12 @@ class CoredumpProblem(ProblemType):
                 frame.function_name = "??"
 
             if db_frame.symbolsource.source_path is not None:
-                frame.source_file = \
-                    db_frame.symbolsource.source_path.encode("ascii", "ignore")
+                if sys.version_info.major == 2:
+                    frame.source_file = \
+                        db_frame.symbolsource.source_path.encode("ascii", "ignore")
+                else:
+                    frame.source_file = \
+                        db_frame.symbolsource.source_path
 
             if db_frame.symbolsource.line_number is not None:
                 frame.source_line = db_frame.symbolsource.line_number

src/pyfaf/retrace.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import threading
 from six.moves import range
 from pyfaf.common import FafError, log
@@ -187,6 +188,10 @@ def addr2line(binary_path, address, debuginfo_dir):
             raise FafError("eu-add2line failed")
 
         line1, line2 = child.stdout.splitlines()
+        if sys.version_info.major == 3:
+            # Python 3
+            line1 = line1.decode("utf-8")
+            line2 = line2.decode("utf-8")
         line2_parts = line2.split(":", 1)
         line2_srcfile = line2_parts[0]
         line2_srcline = int(line2_parts[1])

src/pyfaf/storage/__init__.py
@@ -21,6 +21,7 @@ import errno
 import os
 import pkg_resources
 import six
+import sys
 from six.moves import range
 from pyfaf.common import FafError, log, get_connect_string, import_dir
 from pyfaf.config import config
@@ -123,7 +124,10 @@ class GenericTableBase(object):
             else:
                 raise FafError("Data is too long, '{0}' only allows length of {1}".format(dest.name, maxlen))
 
-        dest.write(data)
+        if sys.version_info.major == 2:
+            dest.write(data)
+        else:
+            dest.write(data.encode("utf-8"))
 
     def _save_lob_file(self, dest, src, maxlen=0, bufsize=4096):
         read = 0
@@ -147,6 +151,8 @@ class GenericTableBase(object):
             mode += "b"
 
         with open(lobpath, mode) as lob:
+            if sys.version_info.major == 3 and isinstance(data, bytes):
+                data = data.decode("utf-8")
             if isinstance(data, six.string_types):
                 self._save_lob_string(lob, data, maxlen, truncate)
             elif hasattr(data, "read"):

src/pyfaf/storage/fixtures/randutils.py
@@ -57,4 +57,4 @@ def randhash():
     '''
     # sha1() function is defined dynamically
     # pylint: disable=E1101
-    return hashlib.sha1(os.urandom(30)).hexdigest()
+    return hashlib.sha1(os.urandom(30).encode("utf-8")).hexdigest()

src/pyfaf/ureport.py
@@ -17,6 +17,7 @@
 # along with faf.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
+import sys
 from sqlalchemy.exc import IntegrityError
 
 from pyfaf.bugtrackers import bugtrackers
@@ -256,7 +257,12 @@ def save_ureport2(db, ureport, create_component=False, timestamp=None, count=1):
 
     db_reportarch.count += count
 
-    reason = ureport["reason"].encode("utf-8")
+    if sys.version_info.major == 2:
+        #Python 2
+        reason = ureport["reason"].encode("utf-8")
+    else:
+        #Python 3
+        reason = ureport["reason"]
     db_reportreason = get_reportreason(db, db_report, reason)
     if db_reportreason is None:
         db_reportreason = ReportReason()

commit 65cbb4f3c2dafc3eed29434058b93ac5c6ec5439

python: Add srcline to stacktraces
Wrong indentation in commit e40e33e177624c830a816df05b3aae8ec8bddf2 caused that line was
removed as part of Python2 cleanup in commit 71aca54f28452ffa957922d712e83b5611156cea

Signed-off-by: Martin Kutlak <mkutlak@redhat.com>

src/pyfaf/problemtypes/python.py
@@ -145,6 +145,9 @@ class PythonProblem(ProblemType):
             frame.file_line = db_frame.symbolsource.offset
             frame.file_name = db_frame.symbolsource.path
 
+            if db_frame.symbolsource.srcline is not None:
+                frame.line_contents = db_frame.symbolsource.srcline
+
             stacktrace.frames.append(frame)
 
         return stacktrace
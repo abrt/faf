c3d23b328628cf472686fc73a1900b758d0d1f17
Matěj Grabovský <mgrabovs@redhat.com>
2020-01-09 11:01:40 +0100

    ureport: Avoid zero unique report counts in summaries
    When saving an incoming report, the number of unique reports in the
    corresponding daily/weekly/monthly summary was incremented if and only
    if the incoming report had 'serial' of 1, i.e. it was the first report of
    its kind from the machine.
    
    However, it may happen that the very first report gets lost, is delayed
    etc., in which case the report's 'serial' field is greater than 1 and
    the uniqe count in the summary is set to 0 if the report is the first of
    its kind on the server.
    
    This is now fixed by setting the unique count in a summary to 1 whenever
    we receive a new report in that period, ignoring its 'serial'.
    
    Lastly, add a database migration which sets the unique count to at least
    one for all the daily/weekly/monthly summaries which claim to have zero
    unique reports, but do have _some_ reports.
    
    Fixes #847

---
+++ src/pyfaf/storage/migrations/versions/9596a0f03838_zero_unique_reports_to_one.py
@@ -0,0 +1,57 @@
+# Copyright (C) 2020  ABRT Team
+# Copyright (C) 2020  Red Hat, Inc.
+#
+# This file is part of faf.
+#
+# faf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# faf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with faf.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Make all report summaries contain at least one unique report
+
+Revision ID: 9596a0f03838
+Revises: fd5dc71471cc
+Create Date: 2020-01-09 09:23:53.053811
+"""
+
+from alembic.op import execute
+from sqlalchemy import and_, update
+
+import pyfaf.storage as st
+
+
+# revision identifiers, used by Alembic.
+revision = '9596a0f03838'
+down_revision = 'fd5dc71471cc'
+
+
+def upgrade():
+    # Set the unique count to one for all report summaries which have no
+    # unique reports but have _some_ reports.
+    execute(update(st.ReportHistoryDaily)
+            .where(and_(st.ReportHistoryDaily.unique == 0,
+                        st.ReportHistoryDaily.count > 0))
+            .values(unique=1))
+    execute(update(st.ReportHistoryMonthly)
+            .where(and_(st.ReportHistoryMonthly.unique == 0,
+                        st.ReportHistoryMonthly.count > 0))
+            .values(unique=1))
+    execute(update(st.ReportHistoryWeekly)
+            .where(and_(st.ReportHistoryWeekly.unique == 0,
+                        st.ReportHistoryWeekly.count > 0))
+            .values(unique=1))
+
+
+def downgrade():
+    # It does not make sense to reverse this migration.
+    pass

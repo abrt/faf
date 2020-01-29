a5ac0d521a94dbb07324f99879dbbbe5e2ebfef0
Michal Fabik <mfabik@redhat.com>
2020-01-29 21:52:29 +0100

    Add wildcard support to repomod
    Allow multiple REPO arguments to faf repomod, using simple enumeration, shell-style wildcards, or a combination thereof.
    
    Example:
    faf repomod repo1 repo2 repo3 --gpgcheck enable
    faf repomod repo* --remove-url file:///some/path
    
    Changing of names (including NICE_NAME) of multiple repositories is not allowed.
    
    Signed-off-by: Michal Fabik <mfabik@redhat.com>

--- src/pyfaf/actions/repomod.py
+++ src/pyfaf/actions/repomod.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with faf.  If not, see <http://www.gnu.org/licenses/>.
 
+import re
+
 import pyfaf.repos
 from pyfaf.actions import Action
 from pyfaf.storage.opsys import Repo, Url, UrlRepo
@@ -29,13 +31,38 @@ class RepoMod(Action):
         self.repo_types = pyfaf.repos.repo_types
 
     def run(self, cmdline, db):
-        repo = (db.session.query(Repo)
-                .filter(Repo.name == cmdline.REPO)
-                .first())
+        if not cmdline.REPO and not cmdline.all:
+            self.log_error("No repositories specified")
+            return 1
 
-        if not repo:
-            self.log_error("Repository '{0}' not found"
-                           .format(cmdline.REPO))
+        repos = []
+
+        for pattern in cmdline.REPO:
+            wildcard_used = any(c in pattern for c in ['?', '*'])
+            if wildcard_used:
+                break
+
+        if (len(cmdline.REPO) > 1 or cmdline.all or wildcard_used) and (cmdline.name or cmdline.nice_name):
+            self.log_error("Can't assign the same name to multiple repositories")
+            return 1
+
+
+        if cmdline.all or '*' in cmdline.REPO:
+            repos.extend(db.session.query(Repo).all())
+
+        else:
+            for pattern in cmdline.REPO:
+                pattern = re.sub('_', r'\\_', pattern)
+                pattern = re.sub('%', r'\\%', pattern)
+                pattern = re.sub(r'\*', '%', pattern)
+                pattern = re.sub(r'\?', '_', pattern)
+                repos.extend(db.session.query(Repo).filter(Repo.name.like(pattern)).all())
+
+        if repos:
+            repos = sorted(list(set(repos)), key=lambda x: x.name)
+
+        else:
+            self.log_warn("No matching repositories found")
             return 1
 
         if cmdline.name:
@@ -45,53 +72,57 @@ class RepoMod(Action):
             if dbrepo:
                 self.log_error("Unable to rename repository '{0}' to '{1}',"
                                " the latter is already defined"
-                               .format(repo.name, dbrepo.name))
+                               .format(cmdline.REPO[0], dbrepo.name))
                 return 1
 
         options = ["name", "type", "nice_name"]
-        for opt in options:
-            if hasattr(cmdline, opt) and getattr(cmdline, opt):
-                newval = getattr(cmdline, opt)
-                self.log_info("Updating {0} on '{1}' to '{2}'"
-                              .format(opt, repo.name, newval))
-                setattr(repo, opt, newval)
+        for repo in repos:
+            for opt in options:
+                if hasattr(cmdline, opt) and getattr(cmdline, opt):
+                    newval = getattr(cmdline, opt)
+                    self.log_info("Updating {0} on '{1}' to '{2}'"
+                                  .format(opt, repo.name, newval))
+                    setattr(repo, opt, newval)
 
-        if cmdline.add_url:
-            new_url = Url()
-            new_url.url = getattr(cmdline, "add_url")
-            repo.url_list.append(new_url)
+            if cmdline.add_url:
+                new_url = Url()
+                new_url.url = getattr(cmdline, "add_url")
+                repo.url_list.append(new_url)
 
-        if cmdline.remove_url:
-            url = (db.session.query(Url)
-                   .join(UrlRepo)
-                   .filter(Url.url == cmdline.remove_url)
-                   .filter(UrlRepo.repo_id == repo.id)
-                   .first())
-            if not url:
-                self.log_error("Url is not assigned to selected repository."
-                               " Use repoinfo to find out more about repository.")
-                return 1
+            if cmdline.remove_url:
+                url = (db.session.query(Url)
+                       .join(UrlRepo)
+                       .filter(Url.url == cmdline.remove_url)
+                       .filter(UrlRepo.repo_id == repo.id)
+                       .first())
+                if not url:
+                    self.log_warn("Url is not assigned to {0} repository."
+                                  " Use repoinfo to find out more about repository.".format(repo.name))
+                    continue
 
-            repo.url_list.remove(url)
-            db.session.delete(url)
+                repo.url_list.remove(url)
+                db.session.delete(url)
 
-        if cmdline.gpgcheck == "enable":
-            repo.nogpgcheck = False
+            if cmdline.gpgcheck == "enable":
+                repo.nogpgcheck = False
 
-        if cmdline.gpgcheck == "disable":
-            repo.nogpgcheck = True
+            if cmdline.gpgcheck == "disable":
+                repo.nogpgcheck = True
 
-        db.session.add(repo)
+            db.session.add(repo)
+            self.log_info("Repository {0} modified".format(repo.name))
+
         db.session.flush()
 
-        self.log_info("Repository modified")
         return 0
 
     def tweak_cmdline_parser(self, parser):
-        parser.add_repo(helpstr="current name of the repository")
+        parser.add_repo(multiple=True, helpstr="current name of the repository")
         parser.add_argument("--name", help="new name of the repository")
         parser.add_repo_type(choices=self.repo_types, helpstr="new type of the repository")
         parser.add_argument("--add-url", help="new repository URL")
         parser.add_argument("--remove-url", help="new repository URL")
         parser.add_argument("--nice-name", help="new human readable name")
         parser.add_gpgcheck_toggle(helpstr="toggle GPG check requirement for this repository")
+        parser.add_argument("-a", "--all", action="store_true", default=False, help="apply to all repositories; "
+                            "do not use with --name or --nice-name")

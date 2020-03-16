6f015e036395998f6633998e92f3b119c3756f79
Ernestas Kulik <ekulik@redhat.com>
2020-03-16 13:16:20 +0100

    Prepare project for use with tito

---
+++ .tito/custom/abrt/__init__.py
@@ -0,0 +1,48 @@
+import os
+import re
+import shlex
+
+from tito.common import info_out, run_command
+from tito.tagger import VersionTagger
+
+
+class Tagger(VersionTagger):
+    def _bump_version(self, release=False, zstream=False):
+        version = super()._bump_version().split('-', maxsplit=1)[0]
+        pattern = re.compile(r'(?<=^m4_define\(\[faf_version\], \[)'
+                             r'.*'
+                             r'(?=\]\))')
+
+        with FileInput(files='configure.ac', inplace=True) as input:
+            for line in input:
+                if pattern.search(line):
+                    line = pattern.sub(version, line)
+
+                print(line, end='')
+
+        return version
+
+    def _tag_release(self):
+        version = self._bump_version()
+
+        self._check_tag_does_not_exist(version)
+        self._clear_package_metadata()
+
+        metadata_file = os.path.join(self.rel_eng_dir, 'packages', self.project_name)
+
+        with open(metadata_file, 'w') as file:
+            file.write('%s %s\n' % (version, self.relative_project_dir))
+
+        files = [
+            metadata_file,
+            os.path.join(self.full_project_dir, self.spec_file_name),
+            'configure.ac',
+        ]
+        run_command('git add -- %s' % (' '.join([shlex.quote(file) for file in files])))
+
+        message = 'Release version %s' % (version)
+
+        run_command('git commit --message="%s"' % message)
+        run_command('git tag --message="%s" %s' % (message, version))
+
+        info_out('%s tagged' % version)
--- autogen.sh
+++ autogen.sh
@@ -1,55 +1,6 @@
 #!/bin/sh
 
-print_help()
-{
-cat << EOH
-Prepares the source tree for configuration
-
-Usage:
-  autogen.sh [sydeps [--install]]
-
-Options:
-
-  sysdeps              prints out all dependencies
-    --install          install all dependencies ('sudo dnf install \$DEPS')
-
-EOH
-}
-
-build_depslist()
-{
-    PACKAGE=$1
-    TEMPFILE=$(mktemp -u --suffix=.spec)
-    sed 's/@PACKAGE_VERSION@/1/' < $PACKAGE.spec.in | sed 's/@.*@//' > $TEMPFILE
-    rpmspec -P $TEMPFILE | grep "^\(Build\)\?Requires:" | \
-        tr -s " " | tr "," "\n" | cut -f2- -d " " | \
-        grep -v "^"$PACKAGE | sort -u | sed -E 's/^(.*) (.*)$/"\1 \2"/' | tr \" \'
-    rm $TEMPFILE
-}
-
-case "$1" in
-    "--help"|"-h")
-            print_help
-            exit 0
-        ;;
-    "sysdeps")
-            DEPS_LIST=$( build_depslist faf)
-
-            if [ "$2" == "--install" ]; then
-                set -x verbose
-                eval sudo dnf install --setopt=strict=0 $DEPS_LIST
-                set +x verbose
-            else
-                echo $DEPS_LIST
-            fi
-            exit 0
-        ;;
-    *)
-            ./gen-version
-            autoreconf --install --force
-            if [ -z "$NOCONFIGURE" ]; then
-                ./configure "$@"
-            fi
-        ;;
-esac
-
+autoreconf --install --force
+if [ -z "$NOCONFIGURE" ]; then
+    ./configure "$@"
+fi

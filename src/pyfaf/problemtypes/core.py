abrt/faf

commit a3e43889b2db7fe5a393ec6ffc6c84a28c0f0a36

logging: Format the debug log messages lazily
Formatting of message arguments is deferred until it cannot be avoided.

See:
https://docs.python.org/3/howto/logging.html#optimization

Signed-off-by: Martin Kutlak <mkutlak@redhat.com>

src/pyfaf/problemtypes/core.py
@@ -466,16 +466,16 @@ class CoredumpProblem(ProblemType):
         return q
 
     def find_packages_for_ssource(self, db, db_ssource):
-        self.log_debug("Build-id: {0}".format(db_ssource.build_id))
+        self.log_debug("Build-id: %d", db_ssource.build_id)
         filename = self._get_file_name_from_build_id(db_ssource.build_id)
-        self.log_debug("File name: {0}".format(filename))
+        self.log_debug("File name: %s", filename)
         db_debug_package = get_package_by_file(db, filename)
         if db_debug_package is None:
             debug_nvra = "Not found"
         else:
             debug_nvra = db_debug_package.nvra()
 
-        self.log_debug("Debug Package: {0}".format(debug_nvra))
+        self.log_debug("Debug Package: %s", debug_nvra)
 
         db_bin_package = None
 
@@ -517,7 +517,7 @@ class CoredumpProblem(ProblemType):
         else:
             bin_nvra = db_bin_package.nvra()
 
-        self.log_debug("Binary Package: {0}".format(bin_nvra))
+        self.log_debug("Binary Package: %s", bin_nvra)
 
         db_src_package = None
 
@@ -530,7 +530,7 @@ class CoredumpProblem(ProblemType):
         else:
             src_nvra = db_src_package.nvra()
 
-        self.log_debug("Source Package: {0}".format(src_nvra))
+        self.log_debug("Source Package: %s", src_nvra)
 
         # indicate incomplete result
         if db_bin_package is None:
@@ -550,10 +550,9 @@ class CoredumpProblem(ProblemType):
             for db_ssource in db_ssources:
                 i += 1
 
-                self.log_debug("[{0} / {1}] Processing '{2}' @ '{3}'"
-                               .format(i, len(db_ssources),
-                                       ssource2funcname(db_ssource),
-                                       db_ssource.path))
+                self.log_debug("[%d / %d] Processing '%s' @ '%s'",
+                               i, len(db_ssources), ssource2funcname(db_ssource),
+                               db_ssource.path)
 
                 norm_path = get_libname(db_ssource.path)
 
@@ -567,8 +566,7 @@ class CoredumpProblem(ProblemType):
                 try:
                     address = get_base_address(binary) + db_ssource.offset
                 except FafError as ex:
-                    self.log_debug("get_base_address failed: {0}"
-                                   .format(str(ex)))
+                    self.log_debug("get_base_address failed: %s", str(ex))
                     db_ssource.retrace_fail_count += 1
                     continue
 
@@ -578,7 +576,7 @@ class CoredumpProblem(ProblemType):
                     results = addr2line(binary, address, debug_path)
                     results.reverse()
                 except Exception as ex: # pylint: disable=broad-except
-                    self.log_debug("addr2line failed: {0}".format(str(ex)))
+                    self.log_debug("addr2line failed: %s", str(ex))
                     db_ssource.retrace_fail_count += 1
                     continue
 
@@ -587,8 +585,7 @@ class CoredumpProblem(ProblemType):
                     inl_id += 1
 
                     funcname, srcfile, srcline = results.pop()
-                    self.log_debug("Unwinding inlined function '{0}'"
-                                   .format(funcname))
+                    self.log_debug("Unwinding inlined function '%s'", funcname)
                     # hack - we have no offset for inlined symbols
                     # let's use minus source line to avoid collisions
                     offset = -srcline
@@ -643,15 +640,14 @@ class CoredumpProblem(ProblemType):
                         db.session.add(db_newframe)
 
                 funcname, srcfile, srcline = results.pop()
-                self.log_debug("Result: {0}".format(funcname))
+                self.log_debug("Result: %s", funcname)
                 db_symbol = get_symbol_by_name_path(db, funcname, norm_path)
                 if db_symbol is None:
                     key = (funcname, norm_path)
                     if key in new_symbols:
                         db_symbol = new_symbols[key]
                     else:
-                        self.log_debug("Creating new symbol '{0}' @ '{1}'"
-                                       .format(funcname, db_ssource.path))
+                        self.log_debug("Creating new symbol '%s' @ '%s'", funcname, db_ssource.path)
                         db_symbol = Symbol()
                         db_symbol.name = funcname
                         db_symbol.normalized_path = norm_path
@@ -667,16 +663,16 @@ class CoredumpProblem(ProblemType):
                 db_ssource.line_number = srcline
 
         if task.debuginfo.unpacked_path is not None:
-            self.log_debug("Removing {0}".format(task.debuginfo.unpacked_path))
+            self.log_debug("Removing %s", task.debuginfo.unpacked_path)
             shutil.rmtree(task.debuginfo.unpacked_path, ignore_errors=True)
 
         if task.source is not None and task.source.unpacked_path is not None:
-            self.log_debug("Removing {0}".format(task.source.unpacked_path))
+            self.log_debug("Removing %s", task.source.unpacked_path)
             shutil.rmtree(task.source.unpacked_path, ignore_errors=True)
 
         for bin_pkg in task.binary_packages.keys():
             if bin_pkg.unpacked_path is not None:
-                self.log_debug("Removing {0}".format(bin_pkg.unpacked_path))
+                self.log_debug("Removing %s", bin_pkg.unpacked_path)
                 shutil.rmtree(bin_pkg.unpacked_path, ignore_errors=True)
 
     def find_crash_function(self, db_backtrace):

src/pyfaf/actions/addcompathashes.py
@@ -115,16 +115,15 @@ class AddCompatHashes(Action):
                 for db_backtrace in db_report.backtraces:
                     k += 1
 
-                    self.log_debug("    [{0} / {1}] Processing backtrace #{2}"
-                                   .format(k, len(db_report.backtraces),
-                                           db_backtrace.id))
+                    self.log_debug("    [%d / %d] Processing backtrace #%d",
+                                   k, len(db_report.backtraces), db_backtrace.id)
                     try:
                         component = db_report.component.name
                         include_offset = ptype.lower() == "python"
                         bthash = self._hash_backtrace(db_backtrace,
                                                       hashbase=[component],
                                                       offset=include_offset)
-                        self.log_debug("    {0}".format(bthash))
+                        self.log_debug("    %s", bthash)
                         db_dup = get_report(db, bthash)
                         if db_dup is None:
                             self.log_info("    Adding hash '{0}'"
@@ -136,8 +135,7 @@ class AddCompatHashes(Action):
                                 db.session.add(db_reporthash)
                                 hashes.add(bthash)
                         elif db_dup == db_report:
-                            self.log_debug("    Hash '{0}' already assigned"
-                                           .format(bthash))
+                            self.log_debug("    Hash '%s' already assigned", bthash)
                         else:
                             self.log_warn(("    Conflict! Skipping hash '{0}'"
                                            " (report #{1})").format(bthash,

src/pyfaf/actions/archive_reports.py
@@ -110,7 +110,7 @@ class ArchiveReports(Action):
             linkpath = os.path.join(tmpsubdir, os.path.basename(filepath))
             # merge - do not overwrite already archived data
             try:
-                self.log_debug("{0} ~> {1}".format(filepath, linkpath))
+                self.log_debug("%s ~> %s", filepath, linkpath)
                 os.symlink(filepath, linkpath)
             except OSError as ex:
                 if ex.errno != errno.EEXIST:

src/pyfaf/actions/c2p.py
@@ -85,7 +85,7 @@ class Coredump2Packages(Action):
                 elif match.group(5) != "-":
                     # vDSO
                     if match.group(7):
-                        self.log_debug("Skipping vDSO {}".format(match.group(8)))
+                        self.log_debug("Skipping vDSO %s", match.group(8))
                         continue
                     # Deleted
                     elif match.group(9):
@@ -118,9 +118,8 @@ class Coredump2Packages(Action):
                               .format(build_id, soname))
                 continue
             else:
-                self.log_debug("Found {0} debuginfo packages for '{1}' ({2}): "
-                               "{3}".format(len(db_packages), build_id, soname,
-                                            [p.nvra() for p in db_packages]))
+                self.log_debug("Found %d debuginfo packages for '%d' (%s): %s",
+                               len(db_packages), build_id, soname, [p.nvra() for p in db_packages])
 
             if build_id not in build_id_maps:
                 build_id_maps[build_id] = set()
@@ -159,13 +158,11 @@ class Coredump2Packages(Action):
             if best["package"]:
                 basename = best["package"].build.base_package_name
                 if basename in Coredump2Packages.SKIP_PACKAGES:
-                    self.log_debug("Skipping '{0}'".format(basename))
+                    self.log_debug("Skipping '%s'", basename)
                     continue
 
-                self.log_debug("Picking '{0}' for '{1}' with {2} build_id "
-                               "matches".format(best["package"].nvra(),
-                                                best["package"].name,
-                                                best["count"]))
+                self.log_debug("Picking '%s' for '%s' with %d build_id matches",
+                               best["package"].nvra(), best["package"].name, best["count"])
 
                 debuginfo_packages.append(best["package"])
                 debuginfo_maps[best["package"].name] = best["package"]
@@ -195,7 +192,7 @@ class Coredump2Packages(Action):
             else:
                 debuginfo_name = build_id_maps[build_id]
                 if debuginfo_name in Coredump2Packages.SKIP_PACKAGES:
-                    self.log_debug("Skipping {0}".format(debuginfo_name))
+                    self.log_debug("Skipping %s", debuginfo_name)
                     continue
 
                 db_arch = debuginfo_maps[debuginfo_name].arch
@@ -245,8 +242,7 @@ class Coredump2Packages(Action):
 
                 for db_package in db_build.packages:
                     if db_package.arch.name == arch:
-                        self.log_debug("Picking {0} for {1}"
-                                       .format(db_package.nvra(), basename))
+                        self.log_debug("Picking %s for %s", db_package.nvra(), basename)
                         result.add(db_package)
 
         link = None

src/pyfaf/actions/create_problems.py
@@ -53,8 +53,7 @@ class CreateProblems(Action):
         empty_problems = get_empty_problems(db)
         self.log_info("Found {0} empty problems".format(len(empty_problems)))
         for db_problem in empty_problems:
-            self.log_debug("Removing empty problem #{0}"
-                           .format(db_problem.id))
+            self.log_debug("Removing empty problem #%d", db_problem.id)
             db.session.delete(db_problem)
         db.session.flush()
 
@@ -170,8 +169,7 @@ class CreateProblems(Action):
             if match > 0:
                 # Ratio of problems matched
                 match_metric = float(match)/len(db_reports)
-                self.log_debug("Found possible match #{0} ({1:.2f})"
-                               .format(db_problem.id, match_metric))
+                self.log_debug("Found possible match #%d (%.2f)", db_problem.id, match_metric)
                 matches.append((match_metric, db_reports, db_problem))
 
         return matches
@@ -199,8 +197,7 @@ class CreateProblems(Action):
         for problem in problems:
             i += 1
 
-            self.log_debug("[{0} / {1}] Processing cluster"
-                           .format(i, len(problems)))
+            self.log_debug("[%d / %d] Processing cluster", i, len(problems))
 
             reports_changed = True
             problem_id = reuse_problems.get(
@@ -209,8 +206,7 @@ class CreateProblems(Action):
                 db_problem = problems_dict.get(problem_id, None)
                 reports_changed = False
                 lookedup_count += 1
-                self.log_debug("Looked up existing problem #{0}"
-                               .format(db_problem.id))
+                self.log_debug("Looked up existing problem #%d", db_problem.id)
             else:
                 matches = self._find_problem_matches(db_problems, problem)
                 if not matches:
@@ -229,7 +225,7 @@ class CreateProblems(Action):
 
         # Phase two: yield problems in order of best match
         self.log_debug("Matching existing problems")
-        self.log_debug("{0} possible matches".format(len(match_list)))
+        self.log_debug("%d possible matches", len(match_list))
         for match_metric, problem, db_problem in sorted(match_list,
                                                         key=itemgetter(0),
                                                         reverse=True):
@@ -247,8 +243,7 @@ class CreateProblems(Action):
             yield (problem, db_problem, True)
 
         # Phase three: create new problems if no match was found above
-        self.log_debug("Processing {0} leftover problems"
-                       .format(len(second_pass)))
+        self.log_debug("Processing %d leftover problems", len(second_pass))
         for problem in second_pass:
             self.log_debug("Creating problem")
             db_problem = Problem()
@@ -256,8 +251,8 @@ class CreateProblems(Action):
             created_count += 1
             yield (problem, db_problem, True)
 
-        self.log_debug("Total: {0}  Looked up: {1}  Found: {2}  Created: {3}"
-                       .format(i, lookedup_count, found_count, created_count))
+        self.log_debug("Total: %d  Looked up: %d  Found: %d  Created: %d",
+                       i, lookedup_count, found_count, created_count)
 
     def _create_problems(self, db, problemplugin, #pylint: disable=too-many-statements
                          report_min_count=0, speedup=False):
@@ -299,8 +294,7 @@ class CreateProblems(Action):
             i = 0
             for db_report in db_reports:
                 i += 1
-                self.log_debug("[{0} / {1}] Loading report #{2}"
-                               .format(i, len(db_reports), db_report.id))
+                self.log_debug("[%d / %d] Loading report #%d", i, len(db_reports), db_report.id)
 
                 _satyr_report = problemplugin.db_report_to_satyr(db_report)
                 if _satyr_report is None:
@@ -322,8 +316,7 @@ class CreateProblems(Action):
             i = 0
             for cluster in clusters:
                 i += 1
-                self.log_debug("[{0} / {1}] Computing distances"
-                               .format(i, len(clusters)))
+                self.log_debug("[%d / %d] Computing distances", i, len(clusters))
                 distances = satyr.Distances(cluster, len(cluster))
 
                 self.log_debug("Getting dendrogram")
@@ -452,8 +445,8 @@ class CreateProblems(Action):
                 if reports_changed:
                     self.update_comps(db, comps, db_problem)
 
-            self.log_debug("Removing {0} invalid reports from problems"
-                           .format(len(invalid_report_ids_to_clean)))
+            self.log_debug("Removing %d invalid reports from problems",
+                           len(invalid_report_ids_to_clean))
             unassign_reports(db, invalid_report_ids_to_clean)
 
             if report_min_count > 0:

src/pyfaf/actions/extfafclonebz.py
@@ -38,11 +38,11 @@ class ExternalFafCloneBZ(Action):
     def _get_bugs(self, url, bz):
         result = set()
 
-        self.log_debug("Opening URL {0}".format(url))
+        self.log_debug("Opening URL %s", url)
         url = urllib.request.urlopen(url)
         code = url.getcode()
         if code != 200:
-            self.log_debug("Unexpected HTTP code: {0}".format(code))
+            self.log_debug("Unexpected HTTP code: %d", code)
             return []
 
         body = url.read()
@@ -58,11 +58,10 @@ class ExternalFafCloneBZ(Action):
                 bug = bz.bz.getbug(bug_id)
                 while (bug.status == "CLOSED" and
                        bug.resolution == "DUPLICATE"):
-                    self.log_debug("Bug {0} is a duplicate of {1}"
-                                   .format(bug.id, bug.dupe_of))
+                    self.log_debug("Bug %d is a duplicate of %d", bug.id, bug.dupe_of)
                     bug = bz.bz.getbug(bug.dupe_of)
             except Exception as ex: # pylint: disable=broad-except
-                self.log_debug("Unable to fetch bug: {0}".format(str(ex)))
+                self.log_debug("Unable to fetch bug: %s", str(ex))
                 continue
 
             result.add(bug)
@@ -108,15 +107,13 @@ class ExternalFafCloneBZ(Action):
                                   db_external_report.external_id))
 
             if db_external_report.report.bz_bugs:
-                self.log_debug("Report #{0} has already a BZ assigned"
-                               .format(db_external_report.report_id))
+                self.log_debug("Report #%d has already a BZ assigned", db_external_report.report_id)
                 continue
 
             now = datetime.datetime.utcnow()
             two_weeks = datetime.timedelta(days=140)
             if now - db_external_report.report.last_occurrence >= two_weeks:
-                self.log_debug("Report #{0} is older than 14 days, skipping"
-                               .format(db_external_report.report_id))
+                self.log_debug("Report #%d is older than 14 days, skipping", db_external_report.report_id)
                 continue
 
             url = "{0}/reports/{1}".format(db_instance.baseurl,
@@ -127,12 +124,11 @@ class ExternalFafCloneBZ(Action):
                 continue
 
             for bug in bugs:
-                self.log_debug("Processing bug #{0}".format(bug.id))
+                self.log_debug("Processing bug #%d", bug.id)
 
                 if bug.component in skip_components:
-                    self.log_debug("Bug #{0} is reported against "
-                                   "an unsupported component {1}"
-                                   .format(bug.id, bug.component))
+                    self.log_debug("Bug #%d is reported against an unsupported component %s",
+                                   bug.id, bug.component)
                     continue
 
                 if bug.status == "CLOSED":

src/pyfaf/actions/extfafdelete.py
@@ -32,7 +32,7 @@ class ExternalFafDelete(Action):
                               "in storage".format(instance_id))
                 continue
 
-            self.log_debug("Processing instance '{0}'".format(db_instance.name))
+            self.log_debug("Processing instance '%s'", db_instance.name)
 
             if db_instance.reports:
                 if not cmdline.cascade:
@@ -42,8 +42,7 @@ class ExternalFafDelete(Action):
                     continue
 
                 for db_external_report in db_instance.reports:
-                    self.log_debug("Deleting assigned report #{0}"
-                                   .format(db_external_report.external_id))
+                    self.log_debug("Deleting assigned report #%d", db_external_report.external_id)
                     db.session.delete(db_external_report)
 
             db.session.delete(db_instance)

src/pyfaf/actions/extfaflink.py
@@ -77,22 +77,19 @@ class ExternalFafLink(Action):
                           .format(i, cnt, db_report.id))
 
             for db_reporthash in db_report.hashes:
-                self.log_debug("Adding report hash '{0}'"
-                               .format(db_reporthash.hash))
+                self.log_debug("Adding report hash '%s'", db_reporthash.hash)
                 hashes.add(db_reporthash.hash)
 
             for db_backtrace in db_report.backtraces:
                 for db_bthash in db_backtrace.hashes:
-                    self.log_debug("Adding backtrace hash '{0}'"
-                                   .format(db_bthash.hash))
+                    self.log_debug("Adding backtrace hash '%s'", db_bthash.hash)
                     hashes.add(db_bthash.hash)
 
             j = 0
             for hashvalue in hashes:
                 j += 1
 
-                self.log_debug("[{0} / {1}] Processing hash '{2}'"
-                               .format(j, len(hashes), hashvalue))
+                self.log_debug("[%d / %d] Processing hash '%s'", j, len(hashes), hashvalue)
                 external_id = self._find_hash(hashvalue,
                                               db_external_faf.baseurl,
                                               parser)
@@ -100,8 +97,7 @@ class ExternalFafLink(Action):
                     continue
 
                 if self._has_external_report(db_report, external_id):
-                    self.log_debug("Skipping existing external report #{0}"
-                                   .format(external_id))
+                    self.log_debug("Skipping existing external report #%d", external_id)
                     continue
 
                 self.log_info("Adding external report #{0}"

commit 8f30d4317d9ba56fab572845f38c80c882348458

pyfaf: core: Use correct format specifier
SymbolSource.build_id is a string. Fallout from
a3e43889b2db7fe5a393ec6ffc6c84a28c0f0a36.

Fixes https://github.com/abrt/faf/issues/910

src/pyfaf/problemtypes/core.py
@@ -469,7 +469,7 @@ class CoredumpProblem(ProblemType):
         return q
 
     def find_packages_for_ssource(self, db, db_ssource):
-        self.log_debug("Build-id: %d", db_ssource.build_id)
+        self.log_debug("Build-id: %s", db_ssource.build_id)
         files = self._build_id_to_debug_files(db_ssource.build_id)
         self.log_debug("File names: %s", ', '.join(files))
         db_debug_package = get_package_by_file(db, files)
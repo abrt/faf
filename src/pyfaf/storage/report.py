42e9ffa1a4cb94fc4b945c9fb91259267718ad4b
Michal Fabik <mfabik@redhat.com>
2020-09-01 17:08:35 +0200

    Postpone evaluation of annotations
    Annotating a function with a return type that is only defined
    later in the code leads to pylint complaints. A similar (but more
    serious) situation arises when two modules try to import from each
    other. This commit introduces the __future__ import that fixes this:
    https://docs.python.org/3.7/whatsnew/3.7.html#pep-563-postponed-evaluation-of-annot
    
    Signed-off-by: Michal Fabik <mfabik@redhat.com>

--- src/pyfaf/storage/report.py
+++ src/pyfaf/storage/report.py
@@ -15,6 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with faf.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import annotations
 
 from collections import namedtuple
 from string import ascii_uppercase #pylint: disable=deprecated-module

f45bb3ef08d3a666030482b317406037a23bd569
Martin Kutlak <mkutlak@users.noreply.github.com>
2020-09-23 18:22:56 +0200

    addcompathashes: Remove import of hashlib
    Commit [0] removed only usage of hashlib and pylint is now complaining about
    unused import.
    
    [0] https://github.com/abrt/faf/commit/75f0d64a8214d3f303434fb8dc3c1cfa0c9d0daf

--- src/pyfaf/actions/addcompathashes.py
+++ src/pyfaf/actions/addcompathashes.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with faf.  If not, see <http://www.gnu.org/licenses/>.
 
-import hashlib
-
 from pyfaf.actions import Action
 from pyfaf.common import FafError
 from pyfaf.problemtypes import problemtypes

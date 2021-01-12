c50f864ff1703d51063166688d29391111b659eb
Michal Fabik <mfabik@redhat.com>
2021-01-12 16:32:23 +0100

    Remove __future__ imports
    Importing annotations from __future__ only works from python3.7 on,
    meaning FAF won't build in RHEL8, i.e. with python3.6
    
    Signed-off-by: Michal Fabik <mfabik@redhat.com>

--- src/pyfaf/storage/problem.py
+++ src/pyfaf/storage/problem.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with faf.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import annotations
-
 from typing import Any, Dict, List
 
 from sqlalchemy.orm import backref, relationship
